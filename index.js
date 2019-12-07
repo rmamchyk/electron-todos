a2b779a77b6da4eeb9716cbedbcde6a76eb0792c
rmamchyk <roman.mamchyk@gmail.com>
2019-12-07 13:41:21 +0200

    added menu bar

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
@@ -10,6 +10,32 @@ app.on('ready', () => {
       nodeIntegration: true
     }
   });
-
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
+
+const isDarwin = process.platform === 'darwin';
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New Todo'
+      },
+      { 
+        label: 'Quit',
+        accelerator: isDarwin ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+if (isDarwin) {
+  menuTemplate.unshift({ label: '' })
+}
