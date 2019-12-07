bdf915874a2af668d4d5fd3c2959e574b14c058f
rmamchyk <roman.mamchyk@gmail.com>
2019-12-07 14:43:43 +0200

    implemented add todo functionality

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const electron = require('electron');
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -11,9 +12,28 @@ app.on('ready', () => {
     }
   });
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+  mainWindow.on('closed', () => app.quit());
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
+});
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    },
+    width: 300,
+    height: 200,
+    title: 'Add New Todo'
+  });
+  addWindow.loadURL(`file://${__dirname}/add.html`);
+  addWindow.on('closed', () => addWindow = null);
+}
+
+ipcMain.on('todo:add', (event, todo) => {
+  mainWindow.webContents.send('todo:add', todo);
+  addWindow.close();
 });
 
 const isDarwin = process.platform === 'darwin';
@@ -23,7 +43,11 @@ const menuTemplate = [
     label: 'File',
     submenu: [
       {
-        label: 'New Todo'
+        label: 'New Todo',
+        accelerator: isDarwin ? 'Command+N' : 'Ctrl+N',
+        click() {
+          createAddWindow();
+        }
       },
       { 
         label: 'Quit',
@@ -38,4 +62,19 @@ const menuTemplate = [
 
 if (isDarwin) {
   menuTemplate.unshift({ label: '' })
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  menuTemplate.push({
+    label: 'Developer',
+    submenu: [
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: isDarwin ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  })
 }
